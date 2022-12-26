e49f33ac8a3f5cde5208e96ad6545458b230adf0
Temitope <toperoyal@gmail.com>
2022-12-26 06:42:22 -0600

    commit 1

---
+++ Square of num folder/square of num.py
@@ -0,0 +1,10 @@
+# FINDING THE SQUARE OF TWO NUMBERS
+numbers = [4,2,6,7,3,5,8,10,6,1,9,2]
+square = []
+sq = 0
+for item in numbers:
+    sq = item**2
+    square.append(sq)
+print("the square of the numbers are :", square)
+
+
---
+++ mypractice.py
@@ -0,0 +1,8 @@
+successful = False
+for number in range(3):
+    print("Attempt")
+    if successful:
+        print("successful")
+        break
+    else:
+        print("Attempt 3 times and fail")
